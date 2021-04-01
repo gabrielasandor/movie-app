03f0ff6e610772580a08d9b7c83a2bbf79f17882
gabrielasandor <71887713+gabrielasandor@users.noreply.github.com>
2021-04-01 10:34:24 +0300

    movies

--- movies-app/src/App.js
+++ movies-app/src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import Movie from './Movie'
-
-
 import {
   BrowserRouter as Router,
   Switch,
@@ -11,8 +9,6 @@ import {
   useParams
 } from "react-router-dom";
 import CircularProgress from '@material-ui/core/CircularProgress'
-
-
 
 require('dotenv').config()
 
@@ -27,14 +23,11 @@ const searchLink = `${url}/search/movie?&api_key=${movieKey}&query=`;
 
 
 <CircularProgress />
-
-
 export default function App() {
   return (
     <Router>
       <div>
         <nav>
-
           <ul>
             <li className="imgli">
               <img className="navimg"
@@ -52,11 +45,12 @@ export default function App() {
           </ul>
         </nav>
 
-        {/* A <Switch> looks through its children <Route>s and
-          renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/ImgId/:id">
             <ActorsFacts />
+          </Route>
+          <Route path="/MovieId/:id">
+            <MovieDescription />
           </Route>
           <Route path="/actors">
             <Actors />
@@ -100,8 +94,8 @@ export default function App() {
                 <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
               </svg>
             </div>
-            <div className="social">
 
+            <div className="social">
             </div>
           </div>
           <div className="right">
@@ -110,12 +104,8 @@ export default function App() {
         </div>
       </footer>
     </Router>
-
-
   );
 }
-
-
 
 function Home() {
   const [movies, setMovie] = useState([]);
@@ -126,19 +116,19 @@ function Home() {
       .then((res) => res.json())
       .then((data) => {
         setMovie(data.results);
+
       });
   }, []);
-
 
   const handleOnSubmit = (e) => {
     e.preventDefault();
 
     if (searchTerm) {
-
       fetch(searchLink + searchTerm)
         .then((res) => res.json())
         .then((data) => {
           setMovie(data.results);
+
           if (data.results.length == 0) {
             fetch(moviesUrl)
               .then((res) => res.json())
@@ -147,23 +137,17 @@ function Home() {
               });
           }
         });
-
       setSearchTerm('')
-
-
-
     }
   };
 
   const handleOnChange = (e) => {
     setSearchTerm(e.target.value);
-
   };
-  
+
   return (
     <div className="root">
       <header>
-
         <form onSubmit={handleOnSubmit}>
           <input
             className="search"
@@ -172,24 +156,51 @@ function Home() {
             value={searchTerm}
             onChange={handleOnChange} />
         </form>
-
       </header>
 
-    
       <div className="movie_content">
-        {movies.length > 0 && movies.map((movie) => <Movie key={movie.id}
-          {...movie} />
+        {movies.length > 0 && movies.map((movie) => <Link to={`/MovieId/${movie.id}`}> <Movie key={movie.id} {...movie} /> </Link>
         )}
-
       </div>
     </div>
   )
-  
+}
+
+function MovieDescription() {
+  const params = useParams();
+  const MovieId = params.id;
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetch(`${url}/movie/${MovieId}?api_key=${movieKey}&language=en-US`)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data);
+        console.log(data);
+
+      });
+  }, []);
+
+  if (movie == null) {
+    return <CircularProgress />
+  }
+  else {
+    return <div >
+      <div className="mainperson">
+        <div className="movie" ><img src={`https://image.tmdb.org/t/p/original${movie.poster_path} `}></img>  </div>
+        <div className="pers_info">
+          <h1>{movie.title}</h1>
+          <h2> Release date: {movie.release_date}</h2>
+          <h2> Duration: {movie.runtime}min</h2>
+          <h2> Summary: {movie.overview}</h2>
+        </div>
+      </div>
+    </div>
+  }
 }
 
 function Upcoming() {
   const [upcoming, setUpcoming] = useState([]);
-
 
   useEffect(() => {
     fetch(upcomingUrl)
@@ -199,26 +210,17 @@ function Upcoming() {
       });
   }, []);
 
-  return (<div className="mainfoto">
-
-    <div className="movie_content">
-
-      {upcoming.map((movie) => <Movie key={movie.id}
-        {...movie} />
+  return (<div className="root">
+<div className="movie_content">
+      {upcoming.map((movie) => <Link to={`/MovieId/${movie.id}`}> <Movie key={movie.id} {...movie} /> </Link>
       )}
     </div>
-
-
-
   </div>
   )
 }
 
-
-
 function Actors() {
   const [persons, setPerson] = useState([]);
-
 
   useEffect(() => {
     fetch(personUrl)
@@ -229,28 +231,19 @@ function Actors() {
       });
   }, []);
 
-  return (<div className="mainfoto">
-
+  return (<div className="root">
     <div className="movie_content">
-
       {persons.length > 0 && persons.map((person) => <div className="movie" key={person.id}><Link to={`/ImgId/${person.id}`}><img src={`https://image.tmdb.org/t/p/original${person.profile_path}`}></img></Link>  <div className="movie_info"> {person.name}</div> </div>
       )}
     </div>
-
-
-
   </div>
   )
 }
 
 function ActorsFacts() {
-
-
   const params = useParams();
   const personId = params.id;
-
   const [person, setPerson] = useState([]);
-
 
   useEffect(() => {
     fetch(`${url}/person/${personId}?api_key=${movieKey}&language=en-US`)
@@ -266,20 +259,12 @@ function ActorsFacts() {
   else {
     return <div >
       <div className="mainperson">
-        <div className="movie" ><img src={`https://image.tmdb.org/t/p/original${person.profile_path} `}></img>  </div>
+        <div className="movie"><img src={`https://image.tmdb.org/t/p/original${person.profile_path} `}></img>  </div>
         <div className="pers_info"> <h1>{person.name}</h1> <h2>Birthday: {person.birthday} </h2>
-
           <h2>   Known for: {person.known_for_department}</h2> <h2>Place of birth: {person.place_of_birth}</h2></div>
-
       </div>
       <div className="biography"><h2>Biography</h2>{person.biography}</div>
-
     </div>
   }
 
 }
-
-
-
-
-
